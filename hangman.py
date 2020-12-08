f5b2c9a333d68d1374b0f80860d86596969b0e4d
Subrhmanya-Devadiga <70996082+Subrhmanya-Devadiga@users.noreply.github.com>
2020-12-08 23:12:12 +0530

    final version

--- hangman.py
+++ hangman.py
@@ -1,24 +1,60 @@
-import time
+from words import word_list
 import random
-import words
+from display_hangman import display_hangman
 
-name = input("Enter your name: ")
-print("Hello! " + name + " Welcome to HangMan game")
-time.sleep(1)
-guess_count = 10
-guess_word = "secret"
-print(guess_word)
-while (guess_count > 0):
-    user_word = input("enter your Guess: ")
-    if user_word == guess_word:
-        time.sleep(1)
-        print("you won! ")
-        break
-    else:
-        guess_count = guess_count - 1
-        time.sleep(1)
-        print("wrong guess! ")
-        print("you have", + guess_count, "left")
-if guess_count == 0:
-    time.sleep(1.5)
-    print("you lost")
+def Random_word():
+    guess_word = random.choice(word_list)
+    return guess_word.upper()
+
+def Play():
+    guess_word = Random_word()
+    word_length = "__ " * len(guess_word)
+    guessed = False
+    guessed_letters = []
+    guessed_word = []
+    guess_count = 6
+    print("\n")
+    name = input("Enter Your name: ")
+    print("Hello " + name.upper() + " welcome to the Hangman_World!")
+    print(display_hangman(guess_count))
+    print(word_length)
+    while not guessed and guess_count > 0:
+        user_word = input("Enter your guess: ").upper()
+        if len(user_word) == 1 and user_word.isalpha():
+            if user_word in guessed_letters:
+                print("you alreday guessed the letter")
+            elif user_word not in guess_word:
+                print(user_word, " not in the word")
+                guess_count -= 1
+                guessed_letters.append(user_word)
+                print(display_hangman(guess_count))
+            else:
+                print("well done, " + user_word + " is in the word")
+                guessed_letters.append(user_word)
+        elif len(user_word) == len(guess_word) and guess_word.isalpha():
+            if user_word == guess_word:
+                print("Wow! correct Guess, Well done")
+                break
+            else:
+                print("your guess is Wrong!")
+                guessed_word.append(user_word)
+                guess_count -= 1
+                print(display_hangman(guess_count))
+        else:
+            print("wrong guess")
+            guessed_word.append(user_word)
+            guess_count -= 1
+            print(display_hangman(guess_count))
+    play_again()
+
+def play_again():
+    replay = input("play again! Y/N : ").upper()
+    if replay == "Y":
+        main()
+
+def main():
+    Random_word()
+    Play()
+
+main()
+
